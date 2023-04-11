0f679685e70a5823bae32269a72bbc4ec39e9d4c
AlekseyUfa1999 <lesha123456789092@gmail.com>
2023-04-11 17:51:30 +0500

    Задание 2

--- 2.py
+++ 2.py
@@ -1,12 +1,16 @@
-# Задача 2. Для списка реализовать обмен значений соседних элементов.
-# Т.е. Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
-# При нечетном количестве элементов последний сохранить на своем месте.
-# Для заполнения списка элементов необходимо использовать функцию input().
-q = int(input("How many items in list do you want to add?\n\t Enter items quantity: "))
-my_lst = []
-for i in range(q):
-    my_lst.append(input(f"Item # {i + 1} : "))
-print(f"Your item list view:\n{my_lst}")
-for x in range(0, (len(my_lst) - 1),2):
-    my_lst[x], my_lst[x+1] = my_lst[x+1], my_lst[x]
-print(f"Your CHANGED item list view:\n{my_lst}")
+'''2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон.
+Функция должна принимать параметры как именованные аргументы.
+Реализовать вывод данных о пользователе одной строкой.'''
+def personal_data(name, lastname, year_of_birth, city, email, phone):
+    return print(f'Имя: {name} Фамилия: {lastname} Год рождения: {year_of_birth}'
+                 f'Город проживания: {city} Email: {email} Телефон: {phone}')
+
+
+personal_data(
+    name=input('Имя: '),
+    lastname=input('Фамилия: '),
+    year_of_birth=input('Год Рождения: '),
+    city=input('Город проживания: '),
+    email=input('email: '),
+    phone=input('Телефон: '),
+)
