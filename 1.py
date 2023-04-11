cb6a520dfc875900a47a2b379779ee75a55b1ac7
AlekseyUfa1999 <lesha123456789092@gmail.com>
2023-04-11 18:07:22 +0500

    Задание 1

--- 1.py
+++ 1.py
@@ -6,8 +6,4 @@ def calculator(a, b):
         return a/b
     except ZeroDivisionError as e:
         print(f'Ошибка! Делить на ноль нельзя')
-
-
-
-
 print(calculator(int(input('Первое число: ')), int(input('Второе число: '))))
